d5be999781c714f33def7fe4953feade8ed2334b
Islam H ALAlfy <61562847+EngALAlfy@users.noreply.github.com>
2023-10-24 17:28:41 +0300

    Add files via upload

---
+++ helper.py
@@ -0,0 +1,26 @@
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+# Line
+LINE = '*' * 50
+
+def print_msg_box(msg, indent=1, width=None, title=None,color=bcolors.BOLD):
+    lines = msg.split('\n')
+    space = " " * indent
+    if not width:
+        width = max(map(len, lines))
+    box = f'╔{"═" * (width + indent * 2)}╗\n'  # upper_border
+    if title:
+        box += f'║{space}{title:<{width}}{space}║\n'  # title
+        box += f'║{space}{"-" * len(title):<{width}}{space}║\n'  # underscore
+    box += ''.join([f'║{space}{line:<{width}}{space}║\n' for line in lines])
+    box += f'╚{"═" * (width + indent * 2)}╝'  # lower_border
+    print(f"{color}{box}{bcolors.ENDC}")
